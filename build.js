a3e01d8a2a867b5c234db177cc60f2e39dcb6b1a
Artin <lengthmin@gmail.com>
2021-07-31 09:10:38 +0000

    refactor: update build.js

--- build.js
+++ build.js
@@ -7,24 +7,27 @@ async function base64_encode(file) {
   var bitmap = await fs.promises.readFile(file);
   return bitmap.toString('base64');
 }
+const resourcesRegex = /^resources\/(.*)$/;
 
 let myPlugin = {
   name: 'res-plugin',
   setup(build) {
-    build.onResolve({ filter: /^resources\/.*$/ }, (args) => {
+    build.onResolve({ filter: resourcesRegex }, args => {
       return {
         path: args.path,
         namespace: 'res-ns',
       };
     });
 
-    build.onLoad({ filter: /.*/, namespace: 'res-ns' }, async (args) => {
-      switch (args.path) {
-        case 'resources/index':
-          let text = await fs.promises.readFile(
-            'src/resources/index.html',
-            'utf8',
-          );
+    build.onLoad({ filter: /.*/, namespace: 'res-ns' }, async args => {
+      const match = resourcesRegex.exec(args.path);
+      const resName = match[1];
+      const resDir = 'src/resources';
+      switch (resName) {
+        case 'index':
+          let text = (
+            await fs.promises.readFile(`${resDir}/index.html`)
+          ).toString();
           let gif = await base64_encode('src/resources/github.gif');
           text = text.replace('%github_gif%', `data:image/gif;base64,${gif}`);
           return {
@@ -32,7 +35,7 @@ let myPlugin = {
             loader: 'text',
           };
         default:
-          let data = await fs.promises.readFile(join('src', args.path), 'utf8');
+          let data = await fs.promises.readFile(join(resDir, resName));
           return {
             contents: data,
             loader: 'text',
@@ -47,19 +50,14 @@ require('esbuild')
     entryPoints: ['./src'],
     bundle: true,
     outfile: './index.js',
-    loader: {
-      '.sh': 'text',
-      '.html': 'text',
-      '.gif': 'dataurl',
-    },
     plugins: [myPlugin],
     minify: true,
     color: true,
     watch: argv['watch'],
   })
-  .then((result) => {
+  .then(result => {
     console.log(result);
   })
-  .catch((e) => {
+  .catch(e => {
     throw e;
   });
